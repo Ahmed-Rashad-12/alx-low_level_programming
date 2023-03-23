42f9a2699a6c0243f7f516a966da7ccafef4c049
Ahmed Rashad <dr.ahmed.rashad@outlook.com>
2023-03-23 14:25:12 -0700

    main file header

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <main.h>
+
+/**
+ * positive_or_negative - check for +ve or -ve #
+ * @i: the number tested
+ * Retun: zero
+ */
+
+void positive_or_negative(int i);
+{
+	if (i < 0)
+		pintf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
